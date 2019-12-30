f3edb8b5ce3f0cfb72a6e19c9f125733b0992ea6
anirbansanu <anirbanmukherjee602@gmail.com>
2019-12-30 20:41:40 +0530

    search gui app

--- info.py
+++ info.py
@@ -1,13 +1,23 @@
 from tkinter import *
+from tkinter.font import Font
+import wikipedia
 
 
 def on_click():
     print("clicked")
+    t.delete(1.0, END)
+    try:
+        ans = wikipedia.summary(e.get())
+        t.insert(INSERT, ans)
+    except:
+        t.insert(INSERT, "Error: Check your internet connection or Input Value")
 
 
 root = Tk()
 root.title("Search")
 root.resizable(width=False, height=False)
+
+t_font = Font(family="Helvetica, Arial, sans-serif", size=12)
 
 f = Frame(root, bg="#de5b6f")
 f.grid(row=0)
@@ -15,7 +25,7 @@ f.grid(row=0)
 e = Entry(f, width=35)
 e.grid(row=1)
 
-b = Button(f, text="Search", command="on_click")
+b = Button(f, text="Search", command=on_click)
 b.grid(row=1, column=1)
 
 f2 = Frame(f)
@@ -24,10 +34,8 @@ f2.grid(row=2, columnspan=2)
 sbar = Scrollbar(f2)
 sbar.pack(side=RIGHT, fill=Y)
 
-
-t = Text(f2, width=50, height=20, fg="black", bg="grey", wrap=WORD, yscrollcommand = sbar.set)
+t = Text(f2, width=50, height=20, fg="black", bg="white", font=t_font, wrap=WORD, yscrollcommand=sbar.set)
 t.pack(side=LEFT)
 sbar.config(command=t.yview())
-
 
 root.mainloop()
